420b58f00d124695b298aa7e6d4510032a981014
MrChaker <mr.chakerw@gmail.com>
2024-07-20 22:28:14 +0100

    Fix asset imports

--- src/components/landingPage/sections/EmploymentSection.tsx
+++ src/components/landingPage/sections/EmploymentSection.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import workers from "../../../assets/workers.webp";
+import shape from "../../../assets/shape.webp";
 
 const EmploymentSection = () => {
   const { t, i18n } = useTranslation();
@@ -16,25 +18,23 @@ const EmploymentSection = () => {
   }, [i18n.language]);
   return (
     <section
-      className={`h-auto w-full flex justify-center items-center font-lato  py-8 `}
-    >
+      className={`h-auto w-full flex justify-center items-center font-lato  py-8 `}>
       <div className="flex max-xl:flex-col max-xl:gap-4 gap-5 items-center max-md:w-full max-md:px-5">
         <img
-          src="/src/assets/workers.webp"
+          src={workers}
           alt="workers"
           className="object-cover h-[363px] w-[345px] "
         />
         <div className="relative max-md:bg-myblue max-md:rounded-2xl max-md:py-2 ">
           <img
-            src="/src/assets/shape.webp"
+            src={shape}
             alt="shape"
             className="max-md:hidden w-[767px] h-auto max-md:w-[300px] "
           />
           <div
             className={`md:absolute top-[60px] left-[75px] z-10 text-center ${
               language == "ar" ? "md:text-right px-7" : ""
-            }`}
-          >
+            }`}>
             <div className="w-full flex flex-col justify-between gap-3 px-4 py-3 max-md:gap-6">
               <h1 className="text-[46px] max-md:text-[24px] max-lg:text-[32px] font-bold text-white ">
                 {t("workersSection.h1")}
@@ -48,8 +48,7 @@ const EmploymentSection = () => {
               <div className="flex justify-center items-center max-md:pt-1 pt-4 ">
                 <button
                   className="bg-primary text-myblack text-[18px] lg:text-[20px]  rounded-3xl lg:w-[222px] w-[150px] py-2 lg:h-[62px] font-semibold font-lato "
-                  onClick={() => navigate("/login")}
-                >
+                  onClick={() => navigate("/login")}>
                   {t("navbar.rejoindre")}
                 </button>
               </div>
--- src/components/landingPage/sections/HeroSection.tsx
+++ src/components/landingPage/sections/HeroSection.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
+import apple from "../../../assets/apple.webp";
+import google from "../../../assets/google.webp";
+import hero_image from "../../../assets/hero_image.webp";
 
 const HeroSection = () => {
   const { t, i18n } = useTranslation();
@@ -15,13 +18,11 @@ const HeroSection = () => {
     <section
       className={`px-4 md:px-20 xl:px-40 h-screen bg-secondary flex ${
         language == "ar" ? "flex-row-reverse" : ""
-      } items-center justify-between font-lato w-full max-lg:flex-col max-lg:gap-7  max-lg:py-4 max-md:gap-5 max-lg:pt-10 pt-0`}
-    >
+      } items-center justify-between font-lato w-full max-lg:flex-col max-lg:gap-7  max-lg:py-4 max-md:gap-5 max-lg:pt-10 pt-0`}>
       <div
         className={`flex flex-col justify-around gap-4 max-md:items-center  ${
           language == "ar" ? "text-right" : ""
-        } `}
-      >
+        } `}>
         <h1 className="text-[46px] max-md:text-[30px] font-semibold text-myblack">
           <Trans i18nKey="hero.h1" />
         </h1>
@@ -29,15 +30,14 @@ const HeroSection = () => {
         <div
           className={`flex flex-1 items-center ${
             language == "ar" ? "justify-end" : ""
-          } gap-5 max-sm:flex-col`}
-        >
-          <img src="/src/assets/apple.webp" alt="" />
-          <img src="/src/assets/google.webp" alt="" />
+          } gap-5 max-sm:flex-col`}>
+          <img src={apple} alt="" />
+          <img src={google} alt="" />
         </div>
       </div>
       <div>
         <img
-          src="/src/assets/hero_image.webp"
+          src={hero_image}
           alt=""
           className="w-full h-auto max-w-[419px] max-h-[589px] md:max-w-[500px]"
         />
